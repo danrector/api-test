3dcc4204a79aa97368d83498d4eb1d8de5a952dc
Daniel Rector <rector.danielk@gmail.com>
2017-02-11 12:18:28 -0500

    added faker to project

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express()
+var faker = require('faker');
 
 app.get('/album/123/assets', function (req, res) {
   
@@ -7,6 +8,7 @@ app.get('/album/123/assets', function (req, res) {
     DO FAKER STUFF HERE, REBUILD SOMETHING THAT LOOKS LIKE THE CHUTE API
   */
   
+  faker.fake("{{name.lastName}}, {{name.firstName}} {{name.suffix}}"));
   
   res.send(FAKE_DATA)
 })

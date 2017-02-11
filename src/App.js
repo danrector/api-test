3dfc56c2822fd887b4560cf70984828edf38a667
Daniel Rector <rector.danielk@gmail.com>
2017-02-11 12:01:25 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      assets: [],
+      isFetchingAssets: false
+    };
+  }
+  
+  componentWillMount() {
+    this.fetchAssets()
+  }
+
+  fetchAssets() {
+    // Set a flag so that we know if we are loading assets
+    // in the render function. Use this to show a loading spinner
+    // or 'Loading...' text.
+    this.setState({isFetchingAssets: true});
+    
+    // Fetch the assets, then cache the assets in state
+    // Set the loading flag to false
+    fetch('https://api.getchute.com/v2/albums/aus6kwrg/assets')
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({
+          assets: responseJson.data,
+          isFetchingAssets: false
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.isFetchingAssets ? this.renderLoadingState() : this.renderAssets()}
+      </div>
+    );
+  }
+  
+  renderLoadingState() {
+    return <span>Loading...</span>
+  }
+  
+  renderAssets() {
+    return (
+      <ul>
+        {this.state.assets.map(asset => (
+          <li>
+            <img src={asset.url} />
+          </li>
+        ))}
+      </ul>
+    )
+  }
+}
+
+export default App;

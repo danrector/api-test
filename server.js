de8b5cd235a8be70302eda85931f266e8ffb3915
Daniel Rector <rector.danielk@gmail.com>
2017-02-17 16:53:50 -0500

    tried a bunch of trouble-shooting shit.

--- server.js
+++ server.js
@@ -2,23 +2,25 @@ var express = require('express')
 var app = express()
 var faker = require('faker');
 
-function generateCustomers () {
-	  var assets = []
+// function generateCustomers () {
+// 	  var people = []
 
-	  for (var id = 0; id < 10; id++) {
-	    var firstName = faker.name.firstName()
-	    var lastName = faker.name.firstName()
-	    var image = faker.image.image()
-	    assets.push({
-	      "id": id,
-	      "first_name": firstName,
-	      "last_name": lastName,
-	      "image": image
-	    })
-	  }
+// 	  for (var id = 0; id < 10; id++) {
+// 	    var firstName = faker.name.firstName()
+// 	    var lastName = faker.name.firstName()
+// 	    var image = faker.image.image()
+// 	    people.push({
+// 	      "id": id,
+// 	      "first_name": firstName,
+// 	      "last_name": lastName,
+// 	      "image": image
+// 	    })
+// 	  }
 
-	  return { "assets": assets }
-	}
+// 	  return { "people": people }
+// 	}
+
+// module.exports = generateCustomers
 
 app.get('/album/123/assets', function (req, res) {
   
@@ -29,10 +31,23 @@ app.get('/album/123/assets', function (req, res) {
 
 	var FAKE_DATA = [];
 
-	FAKE_DATA = generateCustomers ()
+	// FAKE_DATA = generateCustomers ()
+
+	for (i=0; i<=9; i++) {
+
+		var data = {};
+
+		data.name = faker.name.firstName();
+		data.image = faker.image.image();
+
+		FAKE_DATA.push(data);
+	};
+	
+
+		
 	
   
-  res.send(FAKE_DATA)
+  res.send({FAKE_DATA})
 });
 
 app.listen(8081, function () {
